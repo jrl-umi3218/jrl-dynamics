5c39f2ad4db3683e95ab5de65175cdba53a62352
Fumio KANEHIRO <kanehiro@laas.fr>
2008-01-31 11:27:50 +0100

    add typedef for CimplHand

--- include/dynamicsJRLJapan/robotDynamicsImpl.h
+++ include/dynamicsJRLJapan/robotDynamicsImpl.h
@@ -7,6 +7,7 @@
 #include "dynamicsJRLJapan/MultiBody.h"
 #include "dynamicsJRLJapan/DynamicMultiBody.h"
 #include "dynamicsJRLJapan/HumanoidDynamicMultiBody.h"
+#include "dynamicsJRLJapan/Hand.h"
 
 typedef dynamicsJRLJapan::DynamicMultiBody CimplDynamicRobot;
 typedef dynamicsJRLJapan::HumanoidDynamicMultiBody CimplHumanoidDynamicRobot;
@@ -15,5 +16,6 @@ typedef dynamicsJRLJapan::JointRotation CimplJointRotation;
 typedef dynamicsJRLJapan::JointTranslation CimplJointTranslation;
 typedef dynamicsJRLJapan::Joint CimplJoint;
 typedef dynamicsJRLJapan::Body CimplBody;
+typedef dynamicsJRLJapan::Hand CimplHand;
 
 #endif
