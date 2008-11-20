0822fd3a65474621b27dc12700d004a5a0cb7177
Francois Keith <francois.keith@aist.go.jp>
2008-11-20 11:27:34 +0900

    Correct doc

--- include/dynamicsJRLJapan/DynamicMultiBody.h
+++ include/dynamicsJRLJapan/DynamicMultiBody.h
@@ -1074,7 +1074,6 @@ namespace dynamicsJRLJapan
        \li ComputeMomentum: Compute the global momentum and the momentum for each body.
        \li ComputeZMP: Compute ZMP.
        \li ComputeBackwardDynamics: Compute the force and the torque of each joint.
-       \retval 
       */
      bool isSupported(const std::string &inProperty );
        
