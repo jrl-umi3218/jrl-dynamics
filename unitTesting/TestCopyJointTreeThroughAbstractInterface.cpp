7bb998e7d7d82556d5807d6dcf50d26e972645ed
Olivier Stasse <olivier.stasse@aist.go.jp>
2010-01-19 14:04:13 +0900

    Expand copy test to cover hands, feet, specificities and actuated joints.
    In addition some common methods are now shared among several
    tests.

--- unitTesting/TestCopyJointTreeThroughAbstractInterface.cpp
+++ unitTesting/TestCopyJointTreeThroughAbstractInterface.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <fstream>
 #include <dynamicsJRLJapan/dynamicsJRLJapanFactory.h>
+#include "CommonTools.h"
+#include "HumanoidCopy.h"
 
 dynamicsJRLJapan::ObjectFactory robotDynamicsObjectConstructor;
 
@@ -8,220 +10,7 @@ dynamicsJRLJapan::ObjectFactory robotDynamicsObjectConstructor;
 using namespace std;
 using namespace dynamicsJRLJapan;
 
-bool CompareTwoFiles(char *RefFileName, char *OurFileName)
-{
-  ifstream reffile(RefFileName,ifstream::in), 
-    ourfile(OurFileName,ifstream::in);
-  unsigned int NbLine=0;
-  bool readingok=true;
 
-  while(!reffile.eof())
-    {
-
-      char refline[25536],ourline[25536];
-      reffile.getline(refline,25536);
-      ourfile.getline(ourline,25536);
-      if (reffile.gcount()!=ourfile.gcount())
-	readingok=false;
-      else
-	{
-	  for(int i=0;i<reffile.gcount();i++)
-	    if (refline[i]!=ourline[i])
-	      {
-		readingok=false;
-		cerr << "Error at column " << i << endl;
-		break;
-	      }
-	}
-      if (readingok==false)
-	{
-	  cerr << "Error at line "<< NbLine 
-	       << endl << "ref: " << refline 
-	       << endl << "our: " << ourline <<endl;
-	  break;
-	}
-      NbLine++;
-    }
-  return readingok;
-}
-
-/*
-  This function displays an homogeneous matrix as 
-  a 4x4 matrix.
-*/
-
-void DisplayMatrix4x4(matrix4d & todisplay, ostream &os)
-{
-  for(unsigned int i=0;i<4;i++)
-    {
-      for(unsigned int j=0;j<4;j++)
-	{
-	  os << MAL_S4x4_MATRIX_ACCESS_I_J(todisplay,i,j) << " " ;
-	}
-      os << endl;
-    }
-}
-
-void DisplayMatrix4x4AsAVector(matrix4d & todisplay, ostream &os)
-{
-  os << "(";
-  for(unsigned int i=0;i<4;i++)
-    {
-      for(unsigned int j=0;j<4;j++)
-	{
-	  os << MAL_S4x4_MATRIX_ACCESS_I_J(todisplay,i,j) ;
-	  if ((i!=3) || (j!=3))
-	    os << ",";
-	}
-    }
-  os << ")";
-}
-
-void DisplayBody(CjrlBody *aBody,
-		 matrix4d &ct,
-		 ostream &os)
-{
-  matrix3d InertiaMatrix = aBody->inertiaMatrix();
-  
-  os << "Attached body:" << endl;
-  os << "Mass of the attached body: " << aBody->mass() << endl;
-  os << "Local center of mass: " << aBody->localCenterOfMass() << endl;
-  os << "Inertia matrix:" << endl;
-  os << InertiaMatrix << endl;
-  const vector3d lcm3d = aBody->localCenterOfMass();
-  vector4d lcm4d;
-  lcm4d[0] = lcm3d[0]; lcm4d[1] = lcm3d[1];
-  lcm4d[2] = lcm3d[2]; lcm4d[3] = 1.0;
-  vector4d lcmingref;
-  /*  os << "Current Position: ";
-  DisplayMatrix4x4AsAVector(ct,os); 
-  os <<endl;*/
-  MAL_S4x4_C_eq_A_by_B(lcmingref, ct,lcm4d);
-  //  os << "Local center of mass:" << lcmingref <<endl;
-			     
-}
-void DisplayJoint(CjrlJoint * aJoint,
-		  ostream &os)
-{
-  os << " ======================= "<<endl;
-  unsigned int NbCJ = aJoint->countChildJoints();
-  os << "Number of children : " << NbCJ << endl;
-  matrix4d ct = aJoint->currentTransformation();
-  os << "Current Position: ";
-  DisplayMatrix4x4AsAVector(ct,os);		    
-  os << endl;
-  CjrlBody * linkedBody = aJoint->linkedBody();
-  DisplayBody(linkedBody,ct,os);
-}
-
-void RecursiveDisplayOfJoints(CjrlJoint *aJoint,
-			      ostream &os)
-{
-  if (aJoint==0)
-    return;
-
-  int NbChildren = aJoint->countChildJoints();
-
-  DisplayJoint(aJoint,os);
-
-  for(int i=0;i<NbChildren;i++)
-    {
-      // Returns a const so we have to force the casting/
-      RecursiveDisplayOfJoints(aJoint->childJoint(i),os); 
-    }
-
-}
-
-void DisplayHumanoid(CjrlHumanoidDynamicRobot *aHDR,
-		     ostream &os)
-{
-  RecursiveDisplayOfJoints(aHDR->rootJoint(),
-			   os);
-  
-
-  os << "total mass " << aHDR->mass() 
-     << " COM: " << aHDR->positionCenterOfMass() << endl;
-}
-
-void CopyAndInstanciateBody(CjrlJoint *initJoint, 
-			    CjrlJoint *newJoint)
-{
-  CjrlBody * CopiedBody =newJoint->linkedBody();
-  CjrlBody * OriginalBody = initJoint->linkedBody();
-  
-  // Instanciate Copied Body if needed.
-  if (CopiedBody==0)
-    {
-      CopiedBody= robotDynamicsObjectConstructor.createBody();
-      newJoint->setLinkedBody(*CopiedBody);
-    }
-
-  // Copying the mass.
-  double origmass= OriginalBody->mass();
-  CopiedBody->mass(origmass);
-  
-  // Copying the inertia matrix.
-  const matrix3d anInertiaMatrix = OriginalBody->inertiaMatrix();
-  CopiedBody->inertiaMatrix(anInertiaMatrix);
-  
-  // Copying the local center of mass.
-  const vector3d aLocalCenterOfMass = OriginalBody->localCenterOfMass();
-  CopiedBody->localCenterOfMass(aLocalCenterOfMass);
-	  
-}
-void recursiveMultibodyCopy(CjrlJoint *initJoint, CjrlJoint *newJoint)
-{
-  int lNbOfChildren= initJoint->countChildJoints();
-
-  // stop test
-  if (lNbOfChildren == 0) return ;
-
-  for(int li=0;li<lNbOfChildren;li++) {
-    CjrlJoint *Child = initJoint->childJoint(li) ;
-
-    if (Child != 0) {
-      
-      CjrlJoint* a2newJoint=0;
-      const matrix4d pose = Child->initialPosition();
-      a2newJoint = robotDynamicsObjectConstructor.createJointRotation(pose);
-      newJoint->addChildJoint(*a2newJoint);
-      CopyAndInstanciateBody(Child, a2newJoint);
-
-      recursiveMultibodyCopy(Child, a2newJoint) ;
-    }
-  }
-}
-
-
-void PerformCopyFromJointsTree(CjrlHumanoidDynamicRobot* aHDR,
-			       CjrlHumanoidDynamicRobot* a2HDR,
-			       string &JointToRank)
-{
-  CjrlJoint* InitJoint = aHDR->rootJoint() ;
-  
-  CjrlJoint* newJoint=0;
-  matrix4d pose=InitJoint->initialPosition();
-  newJoint = robotDynamicsObjectConstructor.createJointFreeflyer(pose);
-  CopyAndInstanciateBody(InitJoint,newJoint);
-  a2HDR->rootJoint(*newJoint);
-  
-  recursiveMultibodyCopy(InitJoint, newJoint) ;
-
-  string aProperty("FileJointRank");
-  a2HDR->initialize();
-  // Copy the bodies.
-  std::vector<CjrlJoint *> VecOfInitJoints = aHDR->jointVector();
-  std::vector<CjrlJoint *> VecOfCopyJoints = a2HDR->jointVector();
-  
-  if (VecOfInitJoints.size()!=VecOfCopyJoints.size())
-    {
-      std::cout << "Problem while copying the joints. size : "<< VecOfInitJoints.size() 
-		<< " size copy : " << VecOfCopyJoints.size()  << endl;
-      cout << endl << endl << "There is a probleme the new joints vector is not updated" << endl << endl ;
-      exit(-1);
-    }
-
-}
 
 int main(int argc, char *argv[])
 {
@@ -311,11 +100,11 @@ int main(int argc, char *argv[])
   aHDR->computeForwardKinematics();
   
   cout << "Copy First Humanoid Robot" <<endl;
-  PerformCopyFromJointsTree(aHDR, a2HDR,JointToRank);
+  HumanoidCopy aHumanoidCopier;
+  aHumanoidCopier.PerformCopyFromJointsTree(aHDR, a2HDR);
 
   cout << "Copy Second Humanoid Robot" <<endl;
-  PerformCopyFromJointsTree(a2HDR, a3HDR,JointToRank);
-
+  aHumanoidCopier.PerformCopyFromJointsTree(a2HDR, a3HDR);
 
   NbOfDofs = a2HDR->numberDof();
   if (VerboseMode>2)
