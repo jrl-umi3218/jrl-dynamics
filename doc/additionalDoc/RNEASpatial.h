c25a5d079e8dcab127bb7e9d6a0fc0003a1ea136
Olivier Stasse <Olivier.stasse@gmail.com>
2010-09-28 09:58:45 +0900

    Complete algorithm documentation for RNEA Spatial.

--- doc/additionalDoc/RNEASpatial.h
+++ doc/additionalDoc/RNEASpatial.h
@@ -9,7 +9,7 @@
       <ol>
         <li>\f${\bf v}_0 = {\bf 0}\f$ </li>
 	<li>\f${\bf a}_o = -{\bf a}_g\f$</li>
-	<li>\f$for \; i=1\; to \; N_b \; do\f$<br>
+	<li>\f$for \; i=1\; to \; N_b \; then\f$<br>
 	  <ol>
 	    <li>\f${\bf X}_J(i) = xjcalc(jtype(i),{\bf q}_i)\f$</li>
 	    <li>\f${^i{\bf X}}_{p(i)} = {\bf X}_J(i){\bf X}_L(i)\f$</li>
@@ -17,14 +17,30 @@
 	       <ol>
 	         <li> \f${^i{\bf X}_0} = {^i{\bf X}_{p(i)}} {^{p(i)}{\bf X}_0} \f$</li>
 	       </ol>
-            </li>
-	    <li> \f$\text{end}\f$</li>
-	    <li> \f${\bf \Phi}_i = pcalc(jtype(i),{\bf q}_i)\f$</li>
-	    <li> \f${\mathring{\bf \Phi}}c_i = pdcalc(jtype(i),{\bf q}_i)\f$</li>
-	    <li> \f${\bf v}_i = {^i{\bf X}_{p(i)}}{\bf v}_{p(i)} + \Phi_i\dot{q}_i\f$</li>
+	    </li>
+	    <li> \f$ \text{end}\f$</li>
+	    <li> \f$ {\bf \Phi}_i = pcalc(jtype(i),{\bf q}_i)\f$</li>
+	    <li> \f$ {\mathring{\bf \Phi}}c_i = pdcalc(jtype(i),{\bf q}_i)\f$</li>
+	    <li> \f$ {\bf v}_i = {^i{\bf X}_{p(i)}}{\bf v}_{p(i)} + \Phi_i\dot{\bf q}_i\f$</li>
+	    <li> \f$ {\bf \zeta}_i = {\mathring{\bf \Phi}}c_i \dot{\bf q}_i + {\bf v}_i \times \Phi_i\dot{\bf q}_i \f$</li> 
+	    <li> \f$ {\bf a}_i = {^i{\bf X}_{p(i)}} {\bf a}_{p(i)} + \Phi_i \ddot{\bf q}_i + {\bf \zeta}_i \f$</li>
+	    <li> \f$ {\bf f}_i = {\bf I}_i {\bf a}_i + {\bf v}_i \times {\bf I}_i {\bf v}_i \f$</li>
 	  </ol> 
-      </ol>
-	
+	</li>
+	<li> \f$ for \; i=N_b\; to \; 1 \; do \f$
+	  <ol> 
+	    <li>\f$ {\bf \tau}_i = \Phi_i^T {\bf f}_i \f$ </li>
+            <li> \f$ \text{if } p(i)\neq 0 \text{ then } \f$
+               <ol>
+	         <li> \f${\bf f}_p(i) = {\bf f}_{p(i)} + {^i}{\bf X}_{p(i)}^T} \f$ </li>
+	       </ol>
+	    </li>
+	    <li> \f$ \text{end} \f$</li>
+          </ol>
+	</li>
 
+     </ol>
+
+	
       
 */
