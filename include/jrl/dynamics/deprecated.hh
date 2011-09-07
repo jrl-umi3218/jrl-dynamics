681e74e26f6bbffeafbcb1b22a986c77fe8575c6
Florent Lamiraux <florent@laas.fr>
2011-09-07 11:18:02 +0200

    Do not redefind macro JRLDEPRECATED
       Define JRL_DYNAMICS_DEPRECATED instead.

--- include/jrl/dynamics/deprecated.hh
+++ include/jrl/dynamics/deprecated.hh
@@ -1,41 +1,46 @@
-/*
- * Copyright 2008, 2009, 2010,
- *
- * Francois Keith
- * Florent Lamiraux
- *
- * JRL/LAAS, CNRS/AIST
- *
- * This file is part of dynamicsJRLJapan.
- * dynamicsJRLJapan is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * dynamicsJRLJapan is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * You should have received a copy of the GNU Lesser General Public License
- * along with dynamicsJRLJapan.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- */
-
-#ifndef JRL_DEPRECATED_H
-#define JRL_DEPRECATED_H
-
-// This macro allow a generic deprecation of the methods
-// It is based on the code found at
-// http://stackoverflow.com/questions/295120/c-mark-as-deprecated/295154
-#ifdef __GNUC__
-#define JRLDEPRECATED(func) func __attribute__ ((deprecated))
-#elif defined(_MSC_VER)
-#define JRLDEPRECATED(func) __declspec(deprecated) func
-#else
-#pragma message("WARNING: You need to implement JRLDEPRECATED for this compiler")
-#define JRLDEPRECATED(func) func
-#endif
+// Copyright (C) 2010 Florent Lamiraux, Thomas Moulard, JRL, CNRS/AIST.
 //
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#endif
+#ifndef JRL_DYNAMICS_DEPRECATED_HH
+# define JRL_DYNAMICS_DEPRECATED_HH
+
+// Define a suffix which can be used to tag a type, a function or a a
+// variable as deprecated (i.e. it will emit a warning when using it).
+//
+// Tagging a function as deprecated:
+//  void foo () JRL_DYNAMICS_DEPRECATED;
+//
+// Tagging a type as deprecated:
+//  class Foo {};
+//  typedef Foo Bar JRL_DYNAMICS_DEPRECATED;
+//
+// Tagging a variable as deprecated:
+//  int a JRL_DYNAMICS_DEPRECATED = 0;
+//
+// The use of a macro is required as this is /not/ a standardized
+// feature of C++ language or preprocessor, even if most of the
+// compilers support it.
+# ifdef __GNUC__
+#  define JRL_DYNAMICS_DEPRECATED __attribute__ ((deprecated))
+# else
+#  ifdef _MSC_VER
+#   define JRL_DYNAMICS_DEPRECATED __declspec (deprecated)
+#  else
+// If the compiler is not recognized, drop the feature.
+#   define JRL_DYNAMICS_DEPRECATED /* nothing */
+#  endif // __MSVC__
+# endif // __GNUC__
+
+#endif //! JRL_DYNAMICS_DEPRECATED_HH
